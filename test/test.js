b9706b6909e190abd41f075c071e6ea87ff46f1d
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-07-18 17:34:00 +0100

    tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,23 @@
-var test = require('tape')
-var lib = require('../')
+'use strict'
+const test = require('tape')
+const request = require('req-then')
+const LocalWebServer = require('local-web-server')
 
-test('first', function (t) {
-
+test('compress', function (t) {
+  t.plan(2)
+  const ws = new LocalWebServer({
+    stack: [ require('../'), require('local-web-server-static') ],
+    port: 8100,
+    compress: true
+  })
+  ws.listen()
+    .then(() => {
+      request('http://localhost:8100/test/big-file.txt', { headers: { 'Accept-Encoding': 'gzip' } })
+        .then(response => {
+          t.strictEqual(response.res.statusCode, 200)
+          t.strictEqual(response.res.headers['content-encoding'], 'gzip')
+        })
+        .then(ws.close.bind(ws))
+        .catch(err => t.fail('failed: ' + err.stack))
+    })
 })
