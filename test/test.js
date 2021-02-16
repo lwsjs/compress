8b62102575c57e1de900f86240a8a8e896f84c89
Lloyd Brookes <lloyd.brookes@gmail.com>
2021-02-16 20:29:46 +0000

    add specific brotli test

--- test/test.js
+++ test/test.js
@@ -6,14 +6,14 @@ const a = require('assert').strict
 
 const tom = module.exports = new Tom('compress')
 
-tom.test('simple', async function () {
+tom.test('gzip', async function () {
   const port = 8000 + this.index
   class One {
     middleware (options) {
       return async function (ctx, next) {
-        await next()
         const fs = require('fs')
         ctx.body = fs.readFileSync('test/big-file.txt', 'utf8')
+        await next()
       }
     }
   }
@@ -29,4 +29,32 @@ tom.test('simple', async function () {
   })
   lws.server.close()
   a.equal(response.headers.get('vary'), 'Accept-Encoding')
+  a.deepEqual(response.headers.get('content-encoding'), 'gzip')
+})
+
+tom.test('br', async function () {
+  const port = 8000 + this.index
+  class One {
+    middleware (options) {
+      return async function (ctx, next) {
+        const fs = require('fs')
+        ctx.body = fs.readFileSync('test/big-file.txt', 'utf8')
+        await next()
+      }
+    }
+  }
+  const lws = Lws.create({
+    port,
+    stack: [ One, Compress ],
+    compress: true
+  })
+
+  const response = await fetch(`http://localhost:${port}/`, {
+    headers: {
+      'Accept-Encoding': 'br'
+    }
+  })
+  lws.server.close()
+  a.equal(response.headers.get('vary'), 'Accept-Encoding')
+  a.deepEqual(response.headers.get('content-encoding'), 'br')
 })
